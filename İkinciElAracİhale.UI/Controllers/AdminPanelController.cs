3e4264ccbe96194e20e4a0dcea1b1a499d4f6904
pop marley <hsynozguven@hotmail.com>
2023-04-23 18:13:48 +0300

    Eklemeler

--- İkinciElAracİhale.UI/Controllers/AdminPanelController.cs
+++ İkinciElAracİhale.UI/Controllers/AdminPanelController.cs
@@ -47,5 +47,10 @@ namespace İkinciElAracİhale.UI.Controllers
         {
             return View();
         }
+
+        public ActionResult _İhaleFiyat()
+        {
+            return View();
+        }
     }
 }
