f81f13a9dbffb71017308ba28c1a8985747ae699
pop marley <hsynozguven@hotmail.com>
2023-04-23 17:59:52 +0300

    Add project files.

---
+++ İkinciElAracİhale.UI/Controllers/AdminPanelController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace İkinciElAracİhale.UI.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: AdminPanel
+        public ActionResult _AracListeleme()
+        {
+            return View();
+        }
+        public ActionResult _AracDetayBilgisi()
+        {
+            return View();
+        }
+
+        public ActionResult _İlanBilgileri()
+        {
+            return View();
+        }
+
+        public ActionResult _TramerBilgileri()
+        {
+            return View();
+        }
+
+        public ActionResult _PaketTanimlama()
+        {
+            return View();
+        }
+
+        public ActionResult _KullaniciTanimlama()
+        {
+           return View();
+        }
+
+        public ActionResult _İhaleListeleme()
+        {
+            return View();
+        }
+
+        public ActionResult _YeniİhaleOlustur()
+        {
+            return View();
+        }
+    }
+}
---
+++ İkinciElAracİhale.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="İkinciElAracİhale.UI.MvcApplication" Language="C#" %>
